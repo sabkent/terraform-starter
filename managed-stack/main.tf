a7ff1c6d96f6a8d3879c62de46fa2ef2877cf31b
sabkent <sabkent@hotmail.com>
2022-08-06 19:03:25 +0100

    Update main.tf

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -7,4 +7,21 @@ resource "random_password" "secret" {
 
 resource "aws_iam_role" "my_role" {
   name = "my role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
 }
